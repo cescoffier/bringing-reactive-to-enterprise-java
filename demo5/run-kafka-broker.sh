7c499384fd32c73786eae77ebc0bf1d78fd10c17
Clement Escoffier <clement.escoffier@gmail.com>
2018-10-11 11:07:31 +0200

    fix message

--- demo5/run-kafka-broker.sh
+++ demo5/run-kafka-broker.sh
@@ -6,7 +6,7 @@ docker run -d --name zookeeper -p 2181:2181 confluent/zookeeper
 echo "Starting Kafka"
 docker run -d --name kafka -p 9092:9092 --link zookeeper:zookeeper --env KAFKA_ADVERTISED_HOST_NAME=localhost confluent/kafka
 
-echo "Kafka broker started, hit CTRL+C to exit to exit"
+echo "Kafka broker started, hit CTRL+C to exit"
 trap ctrl_c INT
 
 function ctrl_c() {
